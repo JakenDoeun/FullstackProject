9905195f2c10cb74a59a6fd60b1145a77904be1e
Vandoeun <khaovandoeun@gmail.com>
2025-01-02 20:13:45 +0700

    update

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -8,9 +8,9 @@ const methodOverride = require('method-override');
 router.use(methodOverride('_method'));
 
 // Route handlers, using productController methods
-router.get('/houses', productController.getAllProducts); // Get all products
-// router.get('/create', productController.renderCreateForm); // Render create form
-// router.post('/', upload.single('image'), productController.createProduct); // Create product
+router.get('/', productController.getAllProducts); // Get all products
+router.get('/create', productController.renderCreateForm); // Render create form
+router.post('/create', upload.single('image'), productController.createProduct); // Create product
 // router.get('/:id', productController.getProductById); // Show product details
 // router.get('/:id/edit', productController.renderEditForm); // Show edit form
 // router.put('/:id', upload.single('image'), productController.updateProduct); // Update product
